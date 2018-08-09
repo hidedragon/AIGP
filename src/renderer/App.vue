877947efa07629a6a32aad3fb4503df1918a929d
dingdashun <dingdashun@jiuqi.com.cn>
2018-08-10 00:03:09 +0800

    初始化

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sohi'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
